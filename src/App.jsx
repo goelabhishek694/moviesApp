f7ed37d3c91f7f856a9f7ee5e4c144ffc4c82b94
Abhishek <abhishekgoel@gofynd.com>
2024-09-09 09:01:00 +0530

    deploying on netlify

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+import Navbar from './Components/Navbar'
+import Home from './Components/Home'
+import WatchList from './Components/WatchList'
+import {Routes,Route} from 'react-router-dom'
+import Context from '../poc/Context'
+function App() {
+
+  return (
+    <>
+      {/* <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/watchlist" element={<WatchList></WatchList>}></Route>
+      </Routes> */}
+      <Context></Context>
+    </>
+  )
+}
+
+export default App
